5d6c3e880d2c1a275a08beb521765c80c385f3da
thechutrain <chu.chuyalan@gmail.com>
2018-08-29 11:00:05 -0400

    BUG: this.pendingRequests doesn't keep track of promise, after it gets added first time

--- src/webcrawler.js
+++ src/webcrawler.js
@@ -1,3 +1,4 @@
+'use strict';
 const requestPage = require('./utils').requestPage;
 const findPageLinks = require('./utils').findPageLinks;
 const searchPageForText = require('./utils').searchPageForText;
@@ -6,6 +7,85 @@ const MAX_DEPTH = 0; // inclusive value
 
 // TODO: replace this with prompter
 const args = process.argv.slice(2);
+
+function WebCrawler(options = {}) {
+	this.START_URL = options.startUrl || 'https://en.wikipedia.org/wiki/hotdog';
+	this.KEYWORD = options.keyword || 'frankfurter';
+	this.MAX_DEPTH = options.depth || 1;
+	this.MAX_REQ = options.MAX_REQ || 3;
+	this.pendingRequest = Array(this.MAX_REQ).fill(null);
+	this.queuedRequest = [];
+	this.visitedUrls = new Map();
+	this.total_count = 0;
+	this.num_request = 0; // debugging
+	this.init();
+}
+
+WebCrawler.prototype.init = function initWebCrawler() {
+	const url_list = [
+		{
+			url: this.START_URL,
+			depth: 0,
+		},
+	];
+
+	this.crawlPages(url_list);
+};
+
+WebCrawler.prototype.crawlPages = function crawlPages(url_links = []) {
+	// const wc = this;
+	url_links.forEach(
+		function(urlObj) {
+			if (urlObj.depth <= this.MAX_DEPTH) {
+				// Check if room on our pending request array to make request
+				let requestIndex = null;
+				let canMakeRequest = this.pendingRequest.some((e, i) => {
+					if (e === null) {
+						requestIndex = i;
+						return true;
+					}
+				});
+
+				if (canMakeRequest && requestIndex !== null) {
+					//debugging:
+					console.log('--------- Request --------');
+					console.log(this.num_request);
+					console.log(this.pendingRequest);
+
+					let newRequest = requestPage(urlObj.url)
+						.then(
+							function(htmlStr) {
+								this.pendingRequest[requestIndex] = null;
+								// TODO: check if any queuedRequest on page --> pass it
+
+								// TODO: find page occurances
+								this.total_count = searchPageForText(htmlStr, this.KEYWORD);
+
+								// TODO: find page links
+								const newDepth = urlObj.depth + 1;
+								const url_links_2 = findPageLinks(htmlStr).map(url => ({
+									url,
+									depth: newDepth,
+								}));
+								this.crawlPages(url_links_2);
+							}.bind(this)
+						)
+						.catch(e => {
+							console.log(`ERROR in requesPage promise: ${e}`);
+						});
+
+					this.pendingRequest[requestIndex] = newRequest;
+					this.num_request++;
+				} else {
+					this.queuedRequest.push(urlObj);
+				}
+			}
+		}.bind(this)
+	);
+};
+
+// TESTING
+new WebCrawler();
 
 /** ----- searchWikiPages -------
  * given a topic name, makes a request to wikipedia, parses page
@@ -57,4 +137,4 @@ function searchWikiPages(links = url_list, keyword) {
 // --------- TESTING ------------
 // tests that you can make a request to page & prints data
 // const url = args[0];
-searchWikiPages(url_list, 'dog');
+// searchWikiPages(url_list, 'dog');
