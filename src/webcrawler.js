076ff93ad2848d8f0c17ff782948dcb69117ec87
thechutrain <chu.chuyalan@gmail.com>
2018-08-29 13:28:05 -0400

    cleaned up webcrawler, unique urls only, added check for when done

--- src/webcrawler.js
+++ src/webcrawler.js
@@ -32,104 +32,82 @@ WebCrawler.prototype.init = function initWebCrawler() {
 	this.crawlPages(url_list);
 };
 
-WebCrawler.prototype.crawlPages = function crawlPages(url_links = []) {
-	// const wc = this;
-	url_links.forEach(
+WebCrawler.prototype.onEnd = function onEnd() {
+	console.log('finished!');
+};
+
+WebCrawler.prototype.crawlPages = function crawlPages(urlLinks = []) {
+	urlLinks.forEach(
 		function(urlObj) {
-			if (urlObj.depth <= this.MAX_DEPTH) {
-				// Check if room on our pending request array to make request
-				let requestIndex = null;
-				let canMakeRequest = this.pendingRequest.some((e, i) => {
-					if (e === null) {
-						requestIndex = i;
-						return true;
-					}
-				});
+			// Validate depth-level
+			if (urlObj.depth > this.MAX_DEPTH) return;
 
-				if (canMakeRequest && requestIndex !== null) {
-					let newRequest = requestPage(urlObj.url)
-						.then(
-							function(htmlStr) {
-								this.pendingRequest[requestIndex] = null;
-								// TODO: check if any queuedRequest on page --> pass it
+			// Validate unique url only
+			if (this.visitedUrls.has(urlObj.url)) return;
 
-								// TODO: find page occurances
-								this.total_count = searchPageForText(htmlStr, this.KEYWORD);
-
-								// TODO: find page links
-								const newDepth = urlObj.depth + 1;
-								const url_links_2 = findPageLinks(htmlStr).map(url => ({
-									url,
-									depth: newDepth,
-								}));
-								this.crawlPages(url_links_2);
-							}.bind(this)
-						)
-						.catch(e => {
-							console.log(`ERROR in requesPage promise: ${e}`);
-						});
-
-					this.pendingRequest[requestIndex] = newRequest;
-					this.num_request++;
-				} else {
-					this.queuedRequest.push(urlObj);
+			// Check if room on our pending request array to make request
+			let requestIndex = null;
+			let canMakeRequest = this.pendingRequest.some((e, i) => {
+				if (e === null) {
+					requestIndex = i;
+					return true;
 				}
+			});
+
+			if (canMakeRequest && requestIndex !== null) {
+				//#region newRequest Promise
+				let newRequest = requestPage(urlObj.url)
+					.then(
+						function successRequestHandler(htmlStr) {
+							this.pendingRequest[requestIndex] = null;
+
+							// TODO: check if any queuedRequest on page --> pass it
+							// checkQueue(requestIndex)
+
+							// Look for the keyword occurances
+							const keyWordCount = searchPageForText(htmlStr, this.KEYWORD);
+							this.visitedUrls.set(urlObj.url, keyWordCount);
+							this.total_count += keyWordCount;
+
+							// Find next set of external page links
+							const newDepth = urlObj.depth + 1;
+							const nextSetLinks = findPageLinks(htmlStr).map(url => ({
+								url,
+								depth: newDepth,
+							}));
+
+							// Debugging
+							// console.log(nextSetLinks);
+							// console.log(this.pendingRequest);
+
+							this.crawlPages(nextSetLinks);
+
+							// Check if webcrawler is finished
+							const queuedRequestEmpty = this.queuedRequest.length === 0;
+							const pendingRequestEmpty = this.pendingRequest.every(
+								r => r === null
+							);
+							if (queuedRequestEmpty && pendingRequestEmpty) {
+								this.onEnd();
+							}
+						}.bind(this)
+					)
+					.catch(e => {
+						console.log(`ERROR in requesPage promise: ${e}`);
+					});
+				//#endregion newRequest Promise
+
+				this.pendingRequest[requestIndex] = newRequest;
+				this.num_request++;
+				this.visitedUrls.set(urlObj.url, null);
+			} else {
+				this.queuedRequest.push(urlObj);
 			}
 		}.bind(this)
 	);
+
+	// Outside the forEach loop
 };
 
 // TESTING
 new WebCrawler();
-
-/** ----- searchWikiPages -------
- * given a topic name, makes a request to wikipedia, parses page
- * and returns a json object with the most important text
- *
- * @param {string} title
- * @return {Promise} jsonData of the webpage
- */
-//#region searchWikiPages
-
-const url_list = [
-	{
-		url: 'https://en.wikipedia.org/wiki/hotdog',
-		depth: 0,
-	},
-];
-
-let count = 0;
-function searchWikiPages(links = url_list, keyword) {
-	links.forEach(function(urlObj) {
-		if (urlObj.depth <= MAX_DEPTH) {
-			requestPage(urlObj.url)
-				.then(htmlStr => {
-					const newDepth = urlObj.depth + 1;
-
-					// find the links
-					const urlLinks = findPageLinks(htmlStr).map(url => {
-						// TODO: determine if its abs or rel url
-						return { url, depth: newDepth };
-					});
-
-					// find the occurances of some key word
-					count += searchPageForText(htmlStr, keyword);
-
-					searchWikiPages(urlLinks, keyword);
-				})
-				.catch(e => {
-					console.log(`ERROR in requestPage: ${e}`);
-				});
-		} else {
-			console.log(count);
-		}
-	});
-
-	// Check if there are any pending promises
-}
-//#endregion
-
-// --------- TESTING ------------
-// tests that you can make a request to page & prints data
-// const url = args[0];
-// searchWikiPages(url_list, 'dog');
