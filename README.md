109ab89f0304adc0cdde853559a60e5b55e526fc
thechutrain <chu.chuyalan@gmail.com>
2018-09-19 12:07:01 -0400

    got basic pretty printing for one urlList obj done

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Web Crawler
 
 > simple node.js cli web crawler
+
+Looks for the occurance of a key term associated with a webpage and external webpages linked from that page
+
+# Table of Contents
+
+- [Get started]($get-started) \* [Install](#install)
+- [Advanced]($advanced)
+
+# Get started
--- src/webcrawler.js
+++ src/webcrawler.js
@@ -22,40 +22,74 @@ function WebCrawler(options = {}) {
 	this.total_count = 0;
 	this.num_request = 0; // debugging
 
-	// this.example = [
-	// 	{
-	// 		url: this.START_URL,
-	// 		depth: 0,
-	// 		refPage: null, // original page that linked to this url
-	// 		arrUrls: [],
-	// 	},
-	// ];
-	// Begin the program
-	this.init();
-}
-
-// ========= life cycle methods ===========
-// init, onEnd, onError
-//#region lifecycle methods
-WebCrawler.prototype.init = function initWebCrawler() {
-	const url_list = [
+	// Initialize Program:
+	this.inputUrlList = [
 		{
 			url: this.START_URL,
 			depth: 0,
 			refPage: null, // original page that linked to this url
 			arrUrls: [],
+			keyWordCount: 2,
 		},
 	];
+	// this.crawlPages(this.inputUrlList);
+	this.onEnd();
+}
 
-	this.crawlPages(url_list);
-	// this.crawlPages(this.example);
-};
+// ========= life cycle methods ===========
+// init, onEnd, onError
+//#region lifecycle methods
+// WebCrawler.prototype.init = function initWebCrawler() {
+// 	const url_list = [
+// 		{
+// 			url: this.START_URL,
+// 			depth: 0,
+// 			refPage: null, // original page that linked to this url
+// 			arrUrls: [],
+// 		},
+// 	];
+
+// 	this.crawlPages(url_list);
+// 	// this.crawlPages(this.example);
+// };
 
 WebCrawler.prototype.onEnd = function onEnd() {
 	console.log('----------- finished --------');
 	console.log(`Made a total of ${this.num_request} request(s)`);
-	console.log(this.visitedUrls);
-	console.log(this.example);
+
+	this.printResults();
+
+	// Make a printing thing here
+	// let depth = this.inputUrlList[0].depth;
+	// let depth = 1;
+	// let count = 1;
+	// let total = 3;
+	// const prefixSpacer = '|' + Array.apply(null, Array(depth * 2 + 1)).join('-');
+	// const levelText =
+	// 	prefixSpacer +
+	// 	`- Level ${depth}: \n|-- (${count} / ${total})) : www.wiki.com`;
+	// // console.log(levelText);
+
+	// // console.log(this.visitedUrls);
+	// function printUrl(urlList) {
+	// 	let self = this;
+	// 	urlList.forEach(
+	// 		function(urlObj, index, arr) {
+	// 			// let depth = urlObj.depth;
+	// 			const prefixSpacer =
+	// 				'|' + Array.apply(null, Array((urlObj.depth + 1) * 2)).join('-');
+
+	// 			const webHeadText =
+	// 				prefixSpacer + ` (${index + 1}/${arr.length}): ${urlObj.url}`;
+	// 			const resultsFound =
+	// 				prefixSpacer + ` L--> found keyword: ${this.KEYWORD} `;
+
+	// 			console.log(webHeadText);
+	// 		}.bind(this)
+	// 	);
+	// }
+
+	// printUrl(this.inputUrlList);
 };
 
 //TODO: make an on error for promise catch block!
@@ -122,13 +156,15 @@ WebCrawler.prototype._makePageRequest = function _makePageRequest(
 				// Find next set of external page links
 				const newDepth = urlObj.depth + 1;
 				//TODO: instead of getting every url, maybe get urls that don't match current hostname?
-				const nextSetLinks = (urlObj.arrUrls = findPageLinks(htmlStr).map(
-					url => ({
-						url,
-						depth: newDepth,
-						refPage: urlObj.url,
-					})
-				));
+				const nextSetLinks = findPageLinks(htmlStr).map(url => ({
+					url,
+					depth: newDepth,
+					refPage: urlObj.url,
+				}));
+
+				// Update original urlObj
+				urlObj.arrUrls = nextSetLinks;
+				urlObj.keyWordCount = keyWordCount;
 
 				this.crawlPages(nextSetLinks);
 
@@ -161,6 +197,57 @@ WebCrawler.prototype._isDone = function _isDone() {
 	}
 };
 //#endregion
+
+WebCrawler.prototype.printResults = function printResults(
+	urlList = this.inputUrlList
+) {
+	urlList.forEach(
+		function(urlObj, index, arr) {
+			// let depth = urlObj.depth;
+			const prefixDash =
+				'|' + Array.apply(null, Array((urlObj.depth + 1) * 2)).join('-');
+			const prefixSpacer =
+				'|' + Array.apply(null, Array((urlObj.depth + 2) * 2)).join(' ');
+
+			// const depthHeaderText = prefixDash + ` DEPTH of ${urlObj.depth}`;
+			// const webHeadText = prefixSpacer + `| * @url ${urlObj.url}`;
+			// const resultsFound =
+			// 	prefixSpacer +
+			// 	'|' +
+			// 	` * (${index + 1}/${arr.length}) found keyword: ${this.KEYWORD} x${
+			// 		urlObj.keyWordCount
+			// 	}`;
+			// const resultsFound =
+			// 	prefixSpacer +
+			// 	'|' +
+			// 	` * Result ${index + 1} of ${arr.length}, found keyword x${
+			// 		urlObj.keyWordCount
+			// 	} times`;
+
+			// ex. |- Result (1/1) @depth=0
+			const depthHeader = prefixDash + ` DEPTH of ${urlObj.depth}:`;
+			const pageHeader =
+				prefixSpacer +
+				'|' +
+				`- Result (${index + 1}/${arr.length}) @depth=${urlObj.depth}`;
+
+			// ex. | * keyword found: x2 time(s)
+			const countText =
+				prefixSpacer +
+				'|' +
+				` * keyword found: x${urlObj.keyWordCount} time(s)`;
+
+			// TODO: make sure str lenght is less than 30 chars?
+			// ex. | * @url: https://en.wikipedia.org/wiki/hotdog
+			const urlText = prefixSpacer + '|' + ` * @url: ${urlObj.url}`;
+
+			const printStr = [depthHeader, pageHeader, countText, urlText].join('\n');
+			console.log(printStr);
+
+			// console.log(depthHeaderText + '\n' + webHeadText + '\n' + resultsFound);
+		}.bind(this)
+	);
+};
 
 // TESTING
 new WebCrawler();
